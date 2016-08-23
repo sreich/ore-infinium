8c4431e2e6a371f97e19676e8d60693bd7bec001
Shaun Reich <sreich02@gmail.com>
2016-08-23 06:30:17 -0400

    protobuf properly gen'd thru gradle now
    at least when the task is manually invoked.
    also it adds the source sets for IDEA, so it can pick those up

--- core/src/com/ore/infinium/WorldIO.kt
+++ core/src/com/ore/infinium/WorldIO.kt
@@ -1,11 +1,30 @@
 package com.ore.infinium
 
+import java.io.FileOutputStream
+
 class WorldIO(val oreWorld: OreWorld) {
     fun loadWorld() {
     }
 
-    fun saveWorld() {
+    val FILESAVE_BASE_PATH = "../saveData/"
 
+    fun saveWorld() {
+        val fs = FileOutputStream(FILESAVE_BASE_PATH + "worldsave.save").use { fs ->
+            val blocks = PbBlocks.newBuilder()
+            for (y in 0 until OreWorld.WORLD_SIZE_X) {
+                for (x in 0 until OreWorld.WORLD_SIZE_X) {
+                    val blockType = oreWorld.blockType(x,y)
+                    blocks.addBlockTypes(blockType.toInt())
+                }
+            }
+
+            val b = PbWorldSave.newBuilder().setBlocks(blocks).build()
+
+            b.writeTo(fs)
+
+            fs.flush()
+            fs.close()
+        }
     }
 
 }
