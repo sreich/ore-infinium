39dbbfc3355b6fd40374e1268f28447d996cabea
Shaun Reich <sreich02@gmail.com>
2016-09-01 06:11:24 -0400

    starter code for world header writing

--- core/src/com/ore/infinium/WorldIO.kt
+++ core/src/com/ore/infinium/WorldIO.kt
@@ -9,31 +9,47 @@ class WorldIO(val oreWorld: OreWorld) {
     val FILESAVE_BASE_PATH = "../saveData/"
 
     fun saveWorld() {
-        val fs = FileOutputStream(FILESAVE_BASE_PATH + "worldsave.save").use { fs ->
-            val blocks = PbBlocks.newBuilder()
-            for (y in 0 until OreWorld.WORLD_SIZE_X) {
-                for (x in 0 until OreWorld.WORLD_SIZE_X) {
-                    val blockType = oreWorld.blockType(x,y).toInt()
-                    blocks.addBlockTypes(blockType)
+        val saveFilePath = FILESAVE_BASE_PATH + "worldsave.save"
+        val fs = FileOutputStream(saveFilePath).use { fs ->
 
-                    val blockWallType = oreWorld.blockWallType(x,y).toInt()
-                    blocks.addBlockWallTypes(blockWallType)
-
-                    val blockFlags = oreWorld.blockFlags(x,y).toInt()
-                    blocks.addBlockFlags(blockFlags)
-
-                    val blockLightLevel = oreWorld.blockLightLevel(x,y).toInt()
-                    blocks.addBlockLightLevel(blockLightLevel)
-                }
-            }
-
-            val b = PbWorldSave.newBuilder().setBlocks(blocks).build()
-
-            b.writeTo(fs)
-
-            fs.flush()
-            fs.close()
+            //writeWorldHeader(fs)
+            writeWorldData(fs)
         }
+    }
+
+    private fun writeWorldHeader(fs: FileOutputStream) {
+        val header = PbWorldHeader.newBuilder().apply {
+            sizeX = OreWorld.WORLD_SIZE_X
+            sizeY = OreWorld.WORLD_SIZE_Y
+            worldName = "TEST WORLD NAME"
+            worldSeed = 123456789L
+        }.build()
+
+        header.writeTo(fs)
+    }
+
+    private fun writeWorldData(fs: FileOutputStream) {
+        val blocks = PbBlocks.newBuilder()
+        for (y in 0 until OreWorld.WORLD_SIZE_X) {
+            for (x in 0 until OreWorld.WORLD_SIZE_X) {
+                val blockType = oreWorld.blockType(x, y).toInt()
+                blocks.addBlockTypes(blockType)
+
+                val blockWallType = oreWorld.blockWallType(x, y).toInt()
+                blocks.addBlockWallTypes(blockWallType)
+
+                val blockFlags = oreWorld.blockFlags(x, y).toInt()
+                blocks.addBlockFlags(blockFlags)
+
+                val blockLightLevel = oreWorld.blockLightLevel(x, y).toInt()
+                blocks.addBlockLightLevel(blockLightLevel)
+            }
+        }
+
+        val b = PbWorldSave.newBuilder().setBlocks(blocks).build()
+
+        b.writeTo(fs)
+
     }
 
 }
